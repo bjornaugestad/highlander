9259ee6dff5e9c3849f1398aa0e4cd3241030aa0
Bjorn Augestad <bjorn.augestad@gmail.com>
2014-12-06 13:51:32 +0100

    add more doc.

--- meta/src/meta_wlock.h
+++ meta/src/meta_wlock.h
@@ -20,29 +20,19 @@
 #ifndef META_WLOCK_H
 #define META_WLOCK_H
 
-/*
- * The meta_wlock adt implements a waitable/signalable lock
- * (Just a mutex/condvar combo) as one atomic type.
- *
- * We also want to have multiple references to the same pair of mutex/locks
- * so we have a refcount member. Beware of a big giant lock in the
- * implementation of this adt.
- */
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct wlock_tag* wlock;
 
-wlock wlock_new(void);		/* Create a new wlock */
-void  wlock_free(wlock p);	/* destroy a wlock (if refcount == 1) */
-int	  wlock_lock(wlock p);	/* Lock it */
-int	  wlock_unlock(wlock p);/* Unlock it */
-int	  wlock_signal(wlock p);/* Signal it */
-int	  wlock_wait(wlock p);	/* Wait for the wlock to be signalled */
-int	  wlock_broadcast(wlock p);
+wlock wlock_new(void);
+void wlock_free(wlock p);
+int wlock_lock(wlock p);
+int wlock_unlock(wlock p);
+int wlock_signal(wlock p);
+int wlock_wait(wlock p);
+int wlock_broadcast(wlock p);
 
 #ifdef __cplusplus
 }
