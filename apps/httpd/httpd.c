29f15cd12e71ed546f957c29f6ca6486c9a36676
Bjorn Augestad <bjorn@augestad.online>
2018-03-26 07:13:38 +0000

    Add -p portno option

--- apps/httpd/httpd.c
+++ apps/httpd/httpd.c
@@ -19,15 +19,20 @@ static void show_usage(void)
 }
 
 static int m_servertype = SOCKTYPE_TCP;
+static int g_portno = 2000;
 
 static void parse_command_line(int argc, char *argv[])
 {
-    const char *options = "h";
+    const char *options = "hp:";
 
     int c;
 
     while ((c = getopt(argc, argv, options)) != -1) {
         switch (c) {
+            case 'p':
+                g_portno = atoi(optarg);
+                break;
+
             case 'h':
                 show_usage();
                 exit(0);
@@ -39,14 +44,19 @@ static void parse_command_line(int argc, char *argv[])
                 exit(1);
         }
     }
+
+    // Some sanity checks
+    if (g_portno == 0 || g_portno > 65535)
+        die("Port number out of range(%d)\n", g_portno);
 }
+
 int main(int argc, char *argv[])
 {
     http_server s;
     parse_command_line(argc, argv);
 
     s = http_server_new(m_servertype);
-    http_server_set_port(s, 2000);
+    http_server_set_port(s, g_portno);
     if (!http_server_alloc(s))
         die("Could not allocate resources\n");
 
